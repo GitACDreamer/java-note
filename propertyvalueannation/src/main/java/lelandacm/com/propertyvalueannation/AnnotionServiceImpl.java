7af97611d6e8436c712989c861421223c76b194a
sunet_admin <99440227@qq.com>
2020-08-27 18:25:52 +0800

    添加logback日志

--- propertyvalueannation/src/main/java/lelandacm/com/propertyvalueannation/AnnotionServiceImpl.java
+++ propertyvalueannation/src/main/java/lelandacm/com/propertyvalueannation/AnnotionServiceImpl.java
@@ -1,7 +1,6 @@
 package lelandacm.com.propertyvalueannation;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
